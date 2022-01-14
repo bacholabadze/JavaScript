3723580b3a4b1afd9e3a998c46514c5adea98b44
ambri <bachukilabadze@gmail.com>
2022-01-13 20:47:50 -0500

    add working with strings example part3

--- Data Structures, Modern Operators,  Strings/Working With Strings.js
+++ Data Structures, Modern Operators,  Strings/Working With Strings.js
@@ -89,3 +89,45 @@ const checkBaggage = function (items) {
 };
 
 checkBaggage("I have a laptop, some burgers and a pocket knife");
+
+console.log("a+very+nice+string".split("+"));
+// console.log("Bacho Labadze".split(" "));
+
+const [firstname, lastname] = "Bacho Labadze".split(" ");
+// console.log(firstname);
+
+const newName = ["Mr.", firstname, lastname.toUpperCase()].join(" ");
+console.log(newName);
+
+const capitalizeName = function (name) {
+  const names = name.split(" ");
+  const namesUpper = [];
+  for (const n of names) {
+    namesUpper.push(n[0].toUpperCase() + n.slice(1));
+  }
+  console.log(namesUpper.join(" "));
+};
+
+capitalizeName("jessica ann smith davis");
+
+// Padding
+const message = "Go to gate 23!";
+console.log(message.padStart(25, "+").padEnd(36, "+"));
+
+const maskCreditCard = function (number) {
+  const str = number + "";
+  const last = str.slice(-4);
+  return last.padStart(str.length, "*");
+};
+
+console.log(maskCreditCard(9412237381));
+console.log(maskCreditCard("2134037384"));
+
+// Repeat
+const message2 = "Bad weather... All departues delayed... ";
+console.log(message2.repeat(5));
+
+const planesInLine = function (n) {
+  console.log(`There are ${n} planes in line ${"/|\\ ".repeat(n)}`);
+};
+planesInLine(5);
